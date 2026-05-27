d1c0a82866f3e91027db65d91d37b45c789f476f
josedanielfriasruiz-ux <josedanielfriasruiz@gmail.com>
2026-05-27 08:31:25 +0200

    Update upload_pdfs.py

--- facturas/upload_pdfs.py
+++ facturas/upload_pdfs.py
@@ -1,1 +1,21 @@
+import pdfplumber
+import os
 
+CARPETA_PDFS = "facturas"
+
+for archivo in os.listdir(CARPETA_PDFS):
+
+    if archivo.endswith(".pdf"):
+
+        ruta = os.path.join(CARPETA_PDFS, archivo)
+
+        with pdfplumber.open(ruta) as pdf:
+
+            texto = ""
+
+            for pagina in pdf.pages:
+                texto += pagina.extract_text()
+
+        print("FACTURA:", archivo)
+
+        print(texto[:2000])
